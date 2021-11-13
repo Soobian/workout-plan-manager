3e0fe2c32740045de8f86abd7b3c34e88f4076f8
Paulina Gacek <paulina.gacek.pl@gmail.com>
2021-11-13 17:45:36 +0100

    email and password boxes in loginScreen

---
+++ frontend/screens/LoginScreen.js
@@ -0,0 +1,90 @@
+import { useNavigation } from '@react-navigation/core'
+import React, { useEffect, useState } from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+// import { auth } from '../firebase'
+
+const LoginScreen = () => {
+    return(
+        
+        <KeyboardAvoidingView style={styles.container}
+        behavior="padding">
+
+            <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          //value={email}
+          //onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+        
+        <TextInput
+          placeholder="Password"
+          //value={password}
+          //onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+        </KeyboardAvoidingView>
+        
+    )
+  
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+        width: 0,
+        height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    
+    elevation: 6,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
