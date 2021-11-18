a017c05da8cccf67c2a4a99335315f1d2599ca57
Maciej Ciepał <maciejciepal@gmail.com>
2021-11-18 18:44:07 +0100

    Update Bottom navigation bar
    Added @react-navigation package to create navigation between screens

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import HomeScreen from './screens/HomeScreen'
+import React from 'react';
+import { View, Text } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import BottomTab from './components/navigation/BottomTabs';
 
 export default function App() {
   return (
-    <HomeScreen/>
+    <NavigationContainer>
+      <BottomTab />
+    </NavigationContainer>
   )
 }
--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
-import BottomTabs, { bottomTabIcons } from '../components/BottomTabs'
 
 export default function HomeScreen() {
     return (
         <SafeAreaView>
-            <BottomTabs icons={bottomTabIcons}/>
+            <Text>Home</Text>
         </SafeAreaView>
     )
 }
