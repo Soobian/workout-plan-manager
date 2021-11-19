487a32c1de6ce901a1a43086b61b23fb32bbf2ea
Maciej Ciepał <maciejciepal@gmail.com>
2021-11-19 15:34:44 +0100

    Divided "RegisterScreen.js" and "LoginScreen.js" into components
    Moved styles from both files to newly created "RegisterLoginStyles.js" and form to "RegisterLoginForm.js" to reuse code

--- frontend/App.js
+++ frontend/App.js
@@ -9,21 +9,21 @@ import RegisterScreen from './screens/RegisterScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} name="Register" component={RegisterScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-      </Stack.Navigator>
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen options={{ headerShown: false }} name="Register" component={RegisterScreen} />
+                <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+        </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- frontend/screens/LoginScreen.js
+++ frontend/screens/LoginScreen.js
@@ -1,189 +1,28 @@
 import { useNavigation } from '@react-navigation/core'
 import React, { useEffect, useState } from 'react'
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View, TouchableWithoutFeedback, Keyboard } from 'react-native'
-import { FloatingLabelInput } from 'react-native-floating-label-input';
+import RegisterLoginForm from '../components/authentication/RegisterLoginForm';
+
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-  const handleSignUp = () => {
+    formFields = [
+        {label: 'Email', value: email, onChangeText: text => setEmail(text), isPassword: false, secureTextEntry: false,  darkTheme: false},
+        {label: 'Password', value: password, onChangeText: text => setPassword(text), isPassword: true, secureTextEntry: true,  darkTheme: true},
+    ]
+
+    const handleSignUp = () => {
     
-  }
+    }
 
-  const handleLogin = () => {
+    const handleLogin = () => {
     
-  }
-
-
+    }
 
     return(
-    <KeyboardAvoidingView 
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.container}>
-      <View style={styles.upperContener}>
-      <Text style={styles.upperContenerText}>Login to your account</Text>
-      </View>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.inputContainer}>
-          <FloatingLabelInput
-            label={'Email'}
-            value={email}
-            onChangeText={text => setEmail(text)}
-            containerStyles={{
-              backgroundColor: 'white',
-              paddingHorizontal: 15,
-              paddingVertical: 10,
-              borderRadius: 10,
-              marginTop: 15,
-              shadowColor: "#000",
-              shadowOffset: {
-              width: 0,
-              height: 3,
-              },
-              shadowOpacity: 0.27,
-              shadowRadius: 4.65,
-              elevation: 6,
-              }}
-          />
-          <FloatingLabelInput
-            label={'Password'}
-            isPassword
-            darkTheme
-            secureTextEntry
-            value={password}
-            onChangeText={text => setPassword(text)}
-            
-            containerStyles={{
-              backgroundColor: 'white',
-              color: '#03767B',
-              paddingHorizontal: 15,
-              paddingVertical: 10,
-              borderRadius: 10,
-              marginTop: 15,
-              shadowColor: "#000",
-              shadowOffset: {
-              width: 0,
-              height: 3,
-              },
-              shadowOpacity: 0.27,
-              shadowRadius: 4.65,
-              elevation: 6,
-              }}
-          />
-        </View>
-    </TouchableWithoutFeedback>
-    <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          //onPress={handleLogin}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>Sign in</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-      <Text style={styles.smallText}>
-        You don't have an account?
-        <TouchableOpacity>
-          <Text style={styles.buttonLabelText}>
-            Sign up
-          </Text>
-      </TouchableOpacity>
-      </Text>
-    </View>
-    </KeyboardAvoidingView> 
+        <RegisterLoginForm formFields={formFields} title={'Login to your account'} firstText={'Sign in'} secondText={'Sign up'} smallText={"You don't have an account?"}/> 
     )
 }
 
 export default LoginScreen
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  inputContainer: {
-    width: '80%',
-    borderRadius: 20,
-  },
-  input: {
-    backgroundColor: 'white',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderRadius: 10,
-    marginTop: 15,
-    shadowColor: "#000",
-    shadowOffset: {
-        width: 0,
-        height: 3,
-    },
-    shadowOpacity: 0.27,
-    shadowRadius: 4.65,
-    elevation: 6,
-  },
-  buttonContainer: {
-    width: '60%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 40,
-  },
-  button: {
-    backgroundColor: '#03767B',
-    width: '80%',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-  buttonOutline: {
-    backgroundColor: 'white',
-    marginTop: 5,
-    borderColor: '#03767B',
-    borderWidth: 2,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-  buttonOutlineText: {
-    color: '#03767B',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-  upperContener:{
-    color: '#03767B',
-    backgroundColor: '#03767B',
-    width: '103%',
-    height: '40%',
-    fontSize: 16,
-    borderRadius: 10,
-    padding: 10,
-    top: -30,
-  },
-  upperContenerText:{
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 28,
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlignVertical: 'bottom',
-    position: 'absolute',  
-    bottom: 20,
-    left: 30,
-  },
-  smallText:{
-    color: 'gray',
-    fontWeight: '700',
-    fontSize: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlignVertical: 'bottom',
-    marginTop: 20
-  },
-  buttonLabelText:{
-    color: '#03767B',
-    fontWeight: '700',
-    fontSize: 14,
-    marginLeft: 15,
-  }
-})
