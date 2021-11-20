94914f9018b4fb1d4f16f86e8629aa1e4d41bcf7
anetaap <72556989+anetaap@users.noreply.github.com>
2021-11-20 23:39:20 +0100

    authentication tokens

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls'))
+    path('', include('users.urls')),
+    path('auth/', obtain_auth_token),
 ]
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
