183b31845060e3349d342b4f4407f0e0d20656e3
anetaap <72556989+anetaap@users.noreply.github.com>
2021-11-21 14:01:19 +0100

    link to the photo

--- backend/exercise/models.py
+++ backend/exercise/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Exercise(models.Model):
     name = models.TextField(max_length=30, blank=False, null=False)
     description = models.TextField(max_length=500, blank=False, null=False)
+    photo_link = models.TextField(max_length=500, blank=False, null=True)
