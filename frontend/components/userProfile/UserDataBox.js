b86bce355492a70529c4ade69c579acf9a1bb575
paulinagacek <paulina.gacek.pl@gmail.com>
2022-01-06 16:13:30 +0100

    box component with users data

---
+++ frontend/components/userProfile/UserDataBox.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { KeyboardAvoidingView, Text, View, Dimensions, StyleSheet, Image} from 'react-native'
+import { COLORS } from '../colors/Colors';
+const { height, width } = Dimensions.get('window');
+
+const UserDataBox = ({data, navigation}) =>{
+
+    return (
+            <View style={styles.measurementContainer}>
+                <View style={styles.itemContainer}>
+                    <Text style={styles.specificText}>AGE</Text>
+                    <Text style={styles.numbersText}>20</Text>
+                </View>
+                <View style={styles.itemContainer}>
+                    <Text style={styles.specificText}>HEIGHT</Text>
+                    <Text style={styles.numbersText}>{data.height}</Text>
+                </View>
+                <View style={styles.itemContainer}>
+                    <Text style={styles.specificText}>WEIGHT</Text>
+                    <Text style={styles.numbersText}>{data.weight}</Text>
+                </View>
+            </View>
+    )
+}
+
+export default UserDataBox;
+
+const styles = StyleSheet.create({
+        measurementContainer: {
+        top: -height*0.57,
+        width: '80%',
+        borderColor: COLORS.white,
+        borderWidth: 1,
+        borderRadius: 10,
+        height: 60,
+        flexDirection: "row" ,
+        justifyContent: 'space-evenly',
+        backgroundColor: COLORS.white,
+        shadowColor: COLORS.dark_gray,
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 3,
+            },
+            shadowOpacity: 0.27,
+            shadowRadius: 4.65,
+            elevation: 5,
+        paddingHorizontal: 20,
+        marginHorizontal: width*0.1
+    },
+    itemContainer: {
+        height: 50,
+        width: '24%',
+        alignItems: 'center',
+        borderRadius: 8
+    },
+    specificText: {
+        color: COLORS.blue,
+        paddingTop: 7,
+        paddingHorizontal: 5,
+        fontSize: 13,
+        fontWeight: "900",
+        alignItems: 'center'
+    },
+    numbersText : {
+        color: COLORS.blue,
+        fontSize: 19,
+        fontWeight: "900",
+        alignItems: 'center'
+    },
+  })
--- frontend/screens/UserProfileScreen.js
+++ frontend/screens/UserProfileScreen.js
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
-import { KeyboardAvoidingView, Text, View, ScrollView, TouchableOpacity,  Dimensions } from 'react-native'
-import { MeasurementsStyles } from '../components/measurements/MeasurementsStyles';
+import { KeyboardAvoidingView, Text, View, ScrollView, TouchableOpacity, Dimensions, StyleSheet, Image, SafeAreaView} from 'react-native'
 import { COLORS } from '../components/colors/Colors';
+import AvatarWithDetails from '../components/userProfile/AvatarWithDetails';
+import UserDataBox from '../components/userProfile/UserDataBox';
+const { height, width } = Dimensions.get('window');
 
 // avatars: https://www.iconfinder.com/icons/2754576/woman_female_avatar_icon
 
 const UserProfileScreen = ({navigation}) => {
-    const screenWidth = Dimensions.get("window").width;
-    const screenHeight = Dimensions.get("window").height;
+    const [usersData, setUsersData] = useState({
+        name: "random",
+        email: "random@example.com",
+        dateOfBirth: '2137-01-01',
+        height: 200,
+        weight: 130,
+        sex: "m",
+    })
 
     return (
-        <KeyboardAvoidingView 
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            style={MeasurementsStyles.container}>
-            <View style={MeasurementsStyles.upperContainer}>
-                <Text style={MeasurementsStyles.upperContainerText}>My measurements</Text>
-            </View>
-            
-        </KeyboardAvoidingView>
+        <SafeAreaView 
+            style={styles.container}>
+            <AvatarWithDetails data = {usersData} navigation={navigation}/>
+            <UserDataBox data = {usersData} navigation={navigation}/>
+            <Text>elo</Text>
+        </SafeAreaView>
     )
 }
 export default UserProfileScreen
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: COLORS.white,
+        flex: 1,
+        justifyContent: 'center'
+    },
+  })
